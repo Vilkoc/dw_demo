e75befa78d468dad3c9ce507523599fc65835461
Vilkoc <vilkoc@gmail.com>
2020-01-19 19:36:29 +0200

    another fix for heroku

--- datawiz_test/settings.py
+++ datawiz_test/settings.py
@@ -139,7 +139,7 @@ STATIC_URL = '/static/'
 import dj_database_url
 # db_from_env = dj_database_url.config(conn_max_age=500)
 # DATABASES['default'].update(db_from_env)
-DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
 DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 
