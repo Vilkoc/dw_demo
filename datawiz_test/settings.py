77350218020d04e8de45069ae785119537727989
Vilkoc <vilkoc@gmail.com>
2020-01-25 14:49:52 +0200

    another fix for heroku

--- datawiz_test/settings.py
+++ datawiz_test/settings.py
@@ -85,9 +85,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': 'demo',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
+        'USER': 'remote_user',
+        'PASSWORD': '1qaz2dsx',
+        'HOST': 'lstyle.ddns.net',
     }
 }
 
@@ -136,11 +136,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Heroku: Update database configuration fom $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-DATABASES['default'] = dj_database_url.config()
-DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
+# DATABASES['default'] = dj_database_url.config()
+# DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 
 GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
