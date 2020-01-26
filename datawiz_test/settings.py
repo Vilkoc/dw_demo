95437ba14f44360e3a78a88fd741d5d46ac3f564
Vilkoc <vilkoc@gmail.com>
2020-01-26 10:10:33 +0200

    another fix for heroku

--- datawiz_test/settings.py
+++ datawiz_test/settings.py
@@ -134,6 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Heroku: Update database configuration fom $DATABASE_URL.
 import dj_database_url
